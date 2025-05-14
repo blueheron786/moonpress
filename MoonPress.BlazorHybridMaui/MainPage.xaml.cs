52a12770bbd2f17ff27e03583f2aa7cca1037fdb
blueheron786 <blueheron786@gmail.com>
2025-05-14 00:13:35 -0400

    New Blazor Hybrid blazor-maui project
    Retooled to run on Windows only

---
+++ MoonPress.BlazorHybridMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MoonPress.BlazorHybridMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MoonPress.BlazorHybridMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoonPress.BlazorHybridMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
